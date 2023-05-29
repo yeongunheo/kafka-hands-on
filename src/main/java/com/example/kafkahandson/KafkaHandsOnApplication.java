5baf627141e427d1c46860478ae42f866547eb39
yeongun <gjduddns@gmail.com>
2023-05-29 22:09:32 +0900

    git init

---
+++ src/main/java/com/example/kafkahandson/KafkaHandsOnApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkahandson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaHandsOnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaHandsOnApplication.class, args);
+    }
+
+}
